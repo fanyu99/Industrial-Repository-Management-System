bbf90bbdfc22d7baa97a93a62ed036641056cd46
fanyu99 <2273659491@qq.com>
2026-07-14 23:21:48 +0800

    初步完善数据库连接池与RAII包装器

--- WMS/tests/db_test/main.cpp
+++ WMS/tests/db_test/main.cpp
@@ -1,31 +1,48 @@
+#include "qsqldatabase.h"
+#include <QApplication>
 #include <QCoreApplication>
 #include <QDebug>
+#include <QSql>
 #include <QSqlDatabase>
 #include <QSqlError>
 #include <QSqlQuery>
-#include <QSqlResult>
-#include <QString>
-int main(int arc, char* argv[])
+#include <QVector>
+#include <QDate>
+struct Student {
+    int id;
+    QString name;
+    QString subject;
+    int score;
+    QDate exam_date;
+};
+int main(int argc, char* argv[])
 {
-    QCoreApplication app(arc, argv);
-    qDebug() << "Available drivers:" << QSqlDatabase::drivers();
+    QCoreApplication app(argc, argv);
     QSqlDatabase db = QSqlDatabase::addDatabase("QODBC");
-    db.setDatabaseName("DRIVER={MariaDB Unicode};"
-                       "SERVER=127.0.0.1;"
-                       "PORT=3306;"
-                       "DATABASE=wms;"
-                       "UID=fanyu;"
-                       "PWD=862531981;");
-    if (db.open()) {
-        qDebug() << "Database opened successfully";
-        QSqlQuery query(db);
-        if (query.exec("select 1")) {
-            qDebug() << "Query executed successfully";
-        } else {
-            qDebug() << "Query execution failed" << db.lastError().text();
-        }
-    } else {
-        qDebug() << "Database opening failed:" << db.lastError().text();
+    db.setDatabaseName("DRIVER={MariaDB Unicode};SERVER=127.0.0.1;PORT=3306;DATABASE=test;USER=fanyu;PASSWORD=862531981");
+    if (!db.open()) {
+        qDebug() << "数据库连接打开失败" << db.lastError().text();
+        return 1;
     }
-    return 0;
+    qDebug() << "数据库连接打开成功";
+    QSqlQuery query;
+    query.prepare("select * from students where score > 90 and subject='English' order by score desc limit 10");
+    if (!query.exec()) {
+        qDebug() << query.lastError().text();
+        return 1;
+    } else {
+        QVector<Student> students;
+        while (query.next()) {
+            int id = query.value("id").toInt();
+            QString name = query.value("student_name").toString();
+            QString subject=query.value("subject").toString();
+            int score=query.value("score").toInt();
+            QDate exam_date=query.value("exam_date").toDate();
+            students.append({ id,name,subject,score,exam_date });
+        }
+        qDebug()<<"查询到"<<students.size()<<"条数据" << "最终结果(成绩前十):";
+        for (auto stu : students) {
+            qDebug() << "id: " << stu.id << " name: " << stu.name << " subject: " << stu.subject << " score: " << stu.score << " exam_date: " << stu.exam_date;
+        }
+    }
 }
